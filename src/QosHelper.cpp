1b001c444f8b4ae662a2515d33a4383841b99fee
Son Dinh <dinhs@objectcomputing.com>
2024-09-29 17:45:14 -0500

    QoS conversion refactor

--- src/QosHelper.cpp
+++ src/QosHelper.cpp
@@ -2,22 +2,41 @@
 
 #include <cstring>
 
+// Helpers for converting between cxx-generated QoS data structures from the cxx bridge module
+// and the original DDS QoS data structures
 namespace {
+
+// Helper functions to copy a sequence between the DDS and Rust_OpenDDS namespaces
+
+// Used for a scalar type T
+template <typename T>
+void to_dds_value(TAO::unbounded_value_sequence<T>& dds_value, const rust::Vec<T>& cxx_value)
+{
+  const size_t len = cxx_value.size();
+  dds_value.length(len);
+  std::memcpy(dds_value.get_buffer(), cxx_value.data(), len);
+}
+
+template <typename T>
+void to_cxx_value(rust::Vec<T>& cxx_value, const TAO::unbounded_value_sequence<T>& dds_value)
+{
+  const CORBA::ULong len = dds_value.length();
+  cxx_value.reserve(len);
+  for (CORBA::ULong i = 0; i < len; ++i) {
+    cxx_value.push_back(dds_value[i]);
+  }
+}
+
+// Helper functions for converting the nested types.
 
 void to_dds_qos(DDS::TopicDataQosPolicy& dds_qos, const Rust_OpenDDS::TopicDataQosPolicy& cxx_qos)
 {
-  const size_t len = cxx_qos.value.size();
-  dds_qos.value.length(len);
-  std::memcpy(dds_qos.value.get_buffer(), cxx_qos.value.data(), len);
+  to_dds_value(dds_qos.value, cxx_qos.value);
 }
 
 void to_cxx_qos(Rust_OpenDDS::TopicDataQosPolicy& cxx_qos, const DDS::TopicDataQosPolicy& dds_qos)
 {
-  const CORBA::ULong len = dds_qos.value.length();
-  cxx_qos.value.reserve(len);
-  for (CORBA::ULong i = 0; i < len; ++i) {
-    cxx_qos.value.push_back(dds_qos.value[i]);
-  }
+  to_cxx_value(cxx_qos.value, dds_qos.value);
 }
 
 void to_dds_qos(DDS::DurabilityQosPolicyKind& dds_qos, const Rust_OpenDDS::DurabilityQosPolicyKind& cxx_qos)
@@ -184,68 +203,164 @@ void to_cxx_qos(Rust_OpenDDS::LivelinessQosPolicy& cxx_qos, const DDS::Livelines
   to_cxx_qos(cxx_qos.lease_duration, dds_qos.lease_duration);
 }
 
+void to_dds_qos(DDS::ReliabilityQosPolicyKind& dds_qos, const Rust_OpenDDS::ReliabilityQosPolicyKind& cxx_qos)
+{
+  switch (cxx_qos) {
+  case Rust_OpenDDS::ReliabilityQosPolicyKind::BEST_EFFORT_RELIABILITY_QOS:
+    dds_qos = DDS::ReliabilityQosPolicyKind::BEST_EFFORT_RELIABILITY_QOS;
+    break;
+  default:
+    dds_qos = DDS::ReliabilityQosPolicyKind::RELIABLE_RELIABILITY_QOS;
+    break;
+  }
+}
+
+void to_cxx_qos(Rust_OpenDDS::ReliabilityQosPolicyKind& cxx_qos, const DDS::ReliabilityQosPolicyKind& dds_qos)
+{
+  switch (dds_qos) {
+  case DDS::ReliabilityQosPolicyKind::BEST_EFFORT_RELIABILITY_QOS:
+    cxx_qos = Rust_OpenDDS::ReliabilityQosPolicyKind::BEST_EFFORT_RELIABILITY_QOS;
+    break;
+  default:
+    cxx_qos = Rust_OpenDDS::ReliabilityQosPolicyKind::RELIABLE_RELIABILITY_QOS;
+    break;
+  }
+}
+
 void to_dds_qos(DDS::ReliabilityQosPolicy& dds_qos, const Rust_OpenDDS::ReliabilityQosPolicy& cxx_qos)
 {
+  to_dds_qos(dds_qos.kind, cxx_qos.kind);
+  to_dds_qos(dds_qos.max_blocking_time, cxx_qos.max_blocking_time);
 }
 
 void to_cxx_qos(Rust_OpenDDS::ReliabilityQosPolicy& cxx_qos, const DDS::ReliabilityQosPolicy& dds_qos)
 {
+  to_cxx_qos(cxx_qos.kind, dds_qos.kind);
+  to_cxx_qos(cxx_qos.max_blocking_time, dds_qos.max_blocking_time);
+}
+
+void to_dds_qos(DDS::DestinationOrderQosPolicyKind& dds_qos, const Rust_OpenDDS::DestinationOrderQosPolicyKind& cxx_qos)
+{
+  switch (cxx_qos) {
+  case Rust_OpenDDS::DestinationOrderQosPolicyKind::BY_RECEPTION_TIMESTAMP_DESTINATIONORDER_QOS:
+    dds_qos = DDS::DestinationOrderQosPolicyKind::BY_RECEPTION_TIMESTAMP_DESTINATIONORDER_QOS;
+    break;
+  default:
+    dds_qos = DDS::DestinationOrderQosPolicyKind::BY_SOURCE_TIMESTAMP_DESTINATIONORDER_QOS;
+    break;
+  }
+}
+
+void to_cxx_qos(Rust_OpenDDS::DestinationOrderQosPolicyKind& cxx_qos, const DDS::DestinationOrderQosPolicyKind& dds_qos)
+{
+  switch (dds_qos) {
+  case DDS::DestinationOrderQosPolicyKind::BY_RECEPTION_TIMESTAMP_DESTINATIONORDER_QOS:
+    cxx_qos = Rust_OpenDDS::DestinationOrderQosPolicyKind::BY_RECEPTION_TIMESTAMP_DESTINATIONORDER_QOS;
+    break;
+  default:
+    cxx_qos = Rust_OpenDDS::DestinationOrderQosPolicyKind::BY_SOURCE_TIMESTAMP_DESTINATIONORDER_QOS;
+    break;
+  }
 }
 
 void to_dds_qos(DDS::DestinationOrderQosPolicy& dds_qos, const Rust_OpenDDS::DestinationOrderQosPolicy& cxx_qos)
 {
+  to_dds_qos(dds_qos.kind, cxx_qos.kind);
 }
 
 void to_cxx_qos(Rust_OpenDDS::DestinationOrderQosPolicy& cxx_qos, const DDS::DestinationOrderQosPolicy& dds_qos)
 {
+  to_cxx_qos(cxx_qos.kind, dds_qos.kind);
 }
 
 void to_dds_qos(DDS::HistoryQosPolicy& dds_qos, const Rust_OpenDDS::HistoryQosPolicy& cxx_qos)
 {
+  to_dds_qos(dds_qos.kind, cxx_qos.kind);
+  dds_qos.depth = cxx_qos.depth;
 }
 
 void to_cxx_qos(Rust_OpenDDS::HistoryQosPolicy& cxx_qos, const DDS::HistoryQosPolicy& dds_qos)
 {
+  to_cxx_qos(cxx_qos.kind, dds_qos.kind);
+  cxx_qos.depth = dds_qos.depth;
 }
 
 void to_dds_qos(DDS::ResourceLimitsQosPolicy& dds_qos, const Rust_OpenDDS::ResourceLimitsQosPolicy& cxx_qos)
 {
+  dds_qos.max_samples = cxx_qos.max_samples;
+  dds_qos.max_instances = cxx_qos.max_instances;
+  dds_qos.max_samples_per_instance = cxx_qos.max_samples_per_instance;
 }
 
 void to_cxx_qos(Rust_OpenDDS::ResourceLimitsQosPolicy& cxx_qos, const DDS::ResourceLimitsQosPolicy& dds_qos)
 {
+  cxx_qos.max_samples = dds_qos.max_samples;
+  cxx_qos.max_instances = dds_qos.max_instances;
+  cxx_qos.max_samples_per_instance = dds_qos.max_samples_per_instance;
 }
 
 void to_dds_qos(DDS::TransportPriorityQosPolicy& dds_qos, const Rust_OpenDDS::TransportPriorityQosPolicy& cxx_qos)
 {
+  dds_qos.value = cxx_qos.value;
 }
 
 void to_cxx_qos(Rust_OpenDDS::TransportPriorityQosPolicy& cxx_qos, const DDS::TransportPriorityQosPolicy& dds_qos)
 {
+  cxx_qos.value = dds_qos.value;
 }
 
 void to_dds_qos(DDS::LifespanQosPolicy& dds_qos, const Rust_OpenDDS::LifespanQosPolicy& cxx_qos)
 {
+  to_dds_qos(dds_qos.duration, cxx_qos.duration);
 }
 
 void to_cxx_qos(Rust_OpenDDS::LifespanQosPolicy& cxx_qos, const DDS::LifespanQosPolicy& dds_qos)
 {
+  to_cxx_qos(cxx_qos.duration, dds_qos.duration);
+}
+
+void to_dds_qos(DDS::OwnershipQosPolicyKind& dds_qos, const Rust_OpenDDS::OwnershipQosPolicyKind& cxx_qos)
+{
+  switch (cxx_qos) {
+  case Rust_OpenDDS::OwnershipQosPolicyKind::SHARED_OWNERSHIP_QOS:
+    dds_qos = DDS::OwnershipQosPolicyKind::SHARED_OWNERSHIP_QOS;
+    break;
+  default:
+    dds_qos = DDS::OwnershipQosPolicyKind::EXCLUSIVE_OWNERSHIP_QOS;
+    break;
+  }
+}
+
+void to_cxx_qos(Rust_OpenDDS::OwnershipQosPolicyKind& cxx_qos, const DDS::OwnershipQosPolicyKind& dds_qos)
+{
+  switch (dds_qos) {
+  case DDS::OwnershipQosPolicyKind::SHARED_OWNERSHIP_QOS:
+    cxx_qos = Rust_OpenDDS::OwnershipQosPolicyKind::SHARED_OWNERSHIP_QOS;
+    break;
+  default:
+    cxx_qos = Rust_OpenDDS::OwnershipQosPolicyKind::EXCLUSIVE_OWNERSHIP_QOS;
+    break;
+  }
 }
 
 void to_dds_qos(DDS::OwnershipQosPolicy& dds_qos, const Rust_OpenDDS::OwnershipQosPolicy& cxx_qos)
 {
+  to_dds_qos(dds_qos.kind, cxx_qos.kind);
 }
 
 void to_cxx_qos(Rust_OpenDDS::OwnershipQosPolicy& cxx_qos, const DDS::OwnershipQosPolicy& dds_qos)
 {
+  to_cxx_qos(cxx_qos.kind, dds_qos.kind);
 }
 
 void to_dds_qos(DDS::DataRepresentationQosPolicy& dds_qos, const Rust_OpenDDS::DataRepresentationQosPolicy& cxx_qos)
 {
+  to_dds_value(dds_qos.value, cxx_qos.value);
 }
 
 void to_cxx_qos(Rust_OpenDDS::DataRepresentationQosPolicy& cxx_qos, const DDS::DataRepresentationQosPolicy& dds_qos)
 {
+  to_cxx_value(cxx_qos.value, dds_qos.value);
 }
 
 void to_dds_qos(DDS::PresentationQosPolicyAccessScopeKind& dds_qos, const Rust_OpenDDS::PresentationQosPolicyAccessScopeKind& cxx_qos)
@@ -277,136 +392,183 @@ void to_cxx_qos(Rust_OpenDDS::PresentationQosPolicyAccessScopeKind& cxx_qos, con
     break;
   }
 }
+
+void to_dds_qos(DDS::UserDataQosPolicy& dds_qos, const Rust_OpenDDS::UserDataQosPolicy& cxx_qos)
+{
+  to_dds_value(dds_qos.value, cxx_qos.value);
 }
 
-// Helpers for converting between cxx-generated QoS data structures from the cxx bridge module
-// and the original DDS QoS data structures
+void to_cxx_qos(Rust_OpenDDS::UserDataQosPolicy& cxx_qos, const DDS::UserDataQosPolicy& dds_qos)
+{
+  to_cxx_value(cxx_qos.value, dds_qos.value);
+}
+
+void to_dds_qos(DDS::EntityFactoryQosPolicy& dds_qos, const Rust_OpenDDS::EntityFactoryQosPolicy& cxx_qos)
+{
+  dds_qos.autoenable_created_entities = cxx_qos.autoenable_created_entities;
+}
+
+void to_cxx_qos(Rust_OpenDDS::EntityFactoryQosPolicy& cxx_qos, const DDS::EntityFactoryQosPolicy& dds_qos)
+{
+  cxx_qos.autoenable_created_entities = dds_qos.autoenable_created_entities;
+}
+
+void to_dds_qos(TAO::String_Manager& dds_value, const rust::String& cxx_value)
+{
+  dds_value = const_cast<rust::String&>(cxx_value).c_str();
+}
+
+void to_cxx_qos(rust::String& cxx_value, const TAO::String_Manager& dds_value)
+{
+  cxx_value = rust::String(dds_value);
+}
+
+void to_dds_qos(DDS::Property_t& dds_value, const Rust_OpenDDS::Property_t& cxx_value)
+{
+  to_dds_qos(dds_value.name, cxx_value.name);
+  to_dds_qos(dds_value.value, cxx_value.value);
+  dds_value.propagate = cxx_value.propagate;
+}
+
+void to_cxx_qos(Rust_OpenDDS::Property_t& cxx_value, const DDS::Property_t& dds_value)
+{
+  to_cxx_qos(cxx_value.name, dds_value.name);
+  to_cxx_qos(cxx_value.value, dds_value.value);
+  cxx_value.propagate = dds_value.propagate;
+}
+
+void to_dds_qos(DDS::BinaryProperty_t& dds_value, const Rust_OpenDDS::BinaryProperty_t& cxx_value)
+{
+  to_dds_qos(dds_value.name, cxx_value.name);
+  to_dds_value(dds_value.value, cxx_value.value);
+  dds_value.propagate = cxx_value.propagate;
+}
+
+void to_cxx_qos(Rust_OpenDDS::BinaryProperty_t& cxx_value, const DDS::BinaryProperty_t& dds_value)
+{
+  to_cxx_qos(cxx_value.name, dds_value.name);
+  to_cxx_value(cxx_value.value, dds_value.value);
+  cxx_value.propagate = dds_value.propagate;
+}
+
+// T1 and T2 are two equivalent types from the DDS and Rust_OpenDDS namespaces.
+// For example, T1 is DDS::Property_t, T2 is Rust_OpenDDS::Property_t, etc.
+template <typename T1, typename T2>
+void to_dds_value(TAO::unbounded_value_sequence<T1>& dds_value, const rust::Vec<T2>& cxx_value)
+{
+  const size_t len = cxx_value.size();
+  dds_value.length(len);
+  for (size_t i = 0; i < len; ++i) {
+    to_dds_qos(dds_value[i], cxx_value[i]);
+  }
+}
+
+template <typename T1, typename T2>
+void to_cxx_value(rust::Vec<T1>& cxx_value, const TAO::unbounded_value_sequence<T2>& dds_value)
+{
+  const CORBA::ULong len = dds_value.length();
+  cxx_value.reserve(len);
+  for (CORBA::ULong i = 0; i < len; ++i) {
+    T1 tmp;
+    to_cxx_qos(tmp, dds_value[i]);
+    cxx_value.push_back(tmp);
+  }
+}
+
+void to_dds_qos(DDS::PropertyQosPolicy& dds_qos, const Rust_OpenDDS::PropertyQosPolicy& cxx_qos)
+{
+  to_dds_value(dds_qos.value, cxx_qos.value);
+  to_dds_value(dds_qos.binary_value, cxx_qos.binary_value);
+}
+
+void to_cxx_qos(Rust_OpenDDS::PropertyQosPolicy& cxx_qos, const DDS::PropertyQosPolicy& dds_qos)
+{
+  to_cxx_value(cxx_qos.value, dds_qos.value);
+  to_cxx_value(cxx_qos.binary_value, dds_qos.binary_value);
+}
+
+void to_dds_qos(DDS::PresentationQosPolicy& dds_qos, const Rust_OpenDDS::PresentationQosPolicy& cxx_qos)
+{
+  to_dds_qos(dds_qos.access_scope, cxx_qos.access_scope);
+  dds_qos.coherent_access = cxx_qos.coherent_access;
+  dds_qos.ordered_access = cxx_qos.ordered_access;
+}
+
+void to_cxx_qos(Rust_OpenDDS::PresentationQosPolicy& cxx_qos, const DDS::PresentationQosPolicy& dds_qos)
+{
+  to_cxx_qos(cxx_qos.access_scope, dds_qos.access_scope);
+  cxx_qos.coherent_access = dds_qos.coherent_access;
+  cxx_qos.ordered_access = dds_qos.ordered_access;
+}
+
+void to_dds_qos(DDS::StringSeq& dds_value, const rust::Vec<rust::String>& cxx_value)
+{
+  const size_t len = cxx_value.size();
+  dds_value.length(len);
+  for (size_t i = 0; i < len; ++i) {
+    dds_value[i] = const_cast<rust::String&>(cxx_value[i]).c_str();
+  }
+}
+
+void to_cxx_qos(rust::Vec<rust::String>& cxx_value, const DDS::StringSeq& dds_value)
+{
+  const CORBA::ULong len = dds_value.length();
+  cxx_value.reserve(len);
+  for (CORBA::ULong i = 0; i < len; ++i) {
+    cxx_value.emplace_back(dds_value[i]);
+  }
+}
+
+void to_dds_qos(DDS::PartitionQosPolicy& dds_qos, const Rust_OpenDDS::PartitionQosPolicy& cxx_qos)
+{
+  to_dds_qos(dds_qos.name, cxx_qos.name);
+}
+
+void to_cxx_qos(Rust_OpenDDS::PartitionQosPolicy& cxx_qos, const DDS::PartitionQosPolicy& dds_qos)
+{
+  to_cxx_qos(cxx_qos.name, dds_qos.name);
+}
+
+void to_dds_qos(DDS::GroupDataQosPolicy& dds_qos, const Rust_OpenDDS::GroupDataQosPolicy& cxx_qos)
+{
+  to_dds_value(dds_qos.value, cxx_qos.value);
+}
+
+void to_cxx_qos(Rust_OpenDDS::GroupDataQosPolicy& cxx_qos, const DDS::GroupDataQosPolicy& dds_qos)
+{
+  to_cxx_value(cxx_qos.value, dds_qos.value);
+}
+
+} // namespace anonymous
+
 void to_dds_qos(DDS::DomainParticipantQos& dds_qos, const Rust_OpenDDS::DomainParticipantQos& cxx_qos)
 {
-  {
-    const size_t len = cxx_qos.user_data.value.size();
-    dds_qos.user_data.value.length(len);
-    std::memcpy(dds_qos.user_data.value.get_buffer(), cxx_qos.user_data.value.data(), len);
-  }
-
-  dds_qos.entity_factory.autoenable_created_entities = cxx_qos.entity_factory.autoenable_created_entities;
-
-  {
-    const size_t len = cxx_qos.property.value.size();
-    dds_qos.property.value.length(len);
-    for (size_t i = 0; i < len; ++i) {
-      dds_qos.property.value[i].name = cxx_qos.property.value[i].name.data();
-      dds_qos.property.value[i].value = cxx_qos.property.value[i].value.data();
-      dds_qos.property.value[i].propagate = cxx_qos.property.value[i].propagate;
-    }
-  }
-
-  {
-    const size_t len = cxx_qos.property.binary_value.size();
-    dds_qos.property.binary_value.length(len);
-    for (size_t i = 0; i < len; ++i) {
-      dds_qos.property.binary_value[i].name = cxx_qos.property.binary_value[i].name.data();
-      dds_qos.property.binary_value[i].value.length(cxx_qos.property.binary_value[i].value.size());
-      std::memcpy(dds_qos.property.binary_value[i].value.get_buffer(),
-                  cxx_qos.property.binary_value[i].value.data(),
-                  cxx_qos.property.binary_value[i].value.size());
-      dds_qos.property.binary_value[i].propagate = cxx_qos.property.binary_value[i].propagate;
-    }
-  }
+  to_dds_qos(dds_qos.user_data, cxx_qos.user_data);
+  to_dds_qos(dds_qos.entity_factory, cxx_qos.entity_factory);
+  to_dds_qos(dds_qos.property, cxx_qos.property);
 }
 
 void to_cxx_qos(Rust_OpenDDS::DomainParticipantQos& cxx_qos, const DDS::DomainParticipantQos& dds_qos)
 {
-  {
-    const CORBA::ULong len = dds_qos.user_data.value.length();
-    cxx_qos.user_data.value.reserve(len);
-    for (CORBA::ULong i = 0; i < len; ++i) {
-      cxx_qos.user_data.value.push_back(dds_qos.user_data.value[i]);
-    }
-  }
-
-  cxx_qos.entity_factory.autoenable_created_entities = dds_qos.entity_factory.autoenable_created_entities;
-
-  {
-    const CORBA::ULong len = dds_qos.property.value.length();
-    cxx_qos.property.value.reserve(len);
-    for (CORBA::ULong i = 0; i < len; ++i) {
-      Rust_OpenDDS::Property_t tmp;
-      tmp.name = rust::String(dds_qos.property.value[i].name);
-      tmp.value = rust::String(dds_qos.property.value[i].value);
-      tmp.propagate = dds_qos.property.value[i].propagate;
-      cxx_qos.property.value.push_back(tmp);
-    }
-  }
-
-  {
-    const CORBA::ULong len = dds_qos.property.binary_value.length();
-    cxx_qos.property.binary_value.reserve(len);
-    for (CORBA::ULong i = 0; i < len; ++i) {
-      Rust_OpenDDS::BinaryProperty_t tmp;
-      tmp.name = rust::String(dds_qos.property.binary_value[i].name);
-      const CORBA::ULong bin_len = dds_qos.property.binary_value[i].value.length();
-      tmp.value.reserve(bin_len);
-      for (CORBA::ULong j = 0; j < bin_len; ++j) {
-        tmp.value.push_back(dds_qos.property.binary_value[i].value[j]);
-      }
-      tmp.propagate = dds_qos.property.binary_value[i].propagate;
-
-      cxx_qos.property.binary_value.push_back(tmp);
-    }
-  }
+  to_cxx_qos(cxx_qos.user_data, dds_qos.user_data);
+  to_cxx_qos(cxx_qos.entity_factory, dds_qos.entity_factory);
+  to_cxx_qos(cxx_qos.property, dds_qos.property);
 }
 
 void to_dds_qos(DDS::SubscriberQos& dds_qos, const Rust_OpenDDS::SubscriberQos& cxx_qos)
 {
-  // PresentationQosPolicy
-  to_dds_qos(dds_qos.presentation.access_scope, cxx_qos.presentation.access_scope);
-  dds_qos.presentation.coherent_access = cxx_qos.presentation.coherent_access;
-  dds_qos.presentation.ordered_access = cxx_qos.presentation.ordered_access;
-
-  {
-    // PartitionQosPolicy
-    const size_t len = cxx_qos.partition.name.size();
-    dds_qos.partition.name.length(len);
-    for (size_t i = 0; i < len; ++i) {
-      dds_qos.partition.name[i] = cxx_qos.partition.name[i].data();
-    }
-  }
-
-  {
-    // GroupDataQosPolicy
-    const size_t len = cxx_qos.group_data.value.size();
-    dds_qos.group_data.value.length(len);
-    std::memcpy(dds_qos.group_data.value.get_buffer(), cxx_qos.group_data.value.data(), len);
-  }
-
-  // EntityFactoryQosPolicy
-  dds_qos.entity_factory.autoenable_created_entities = cxx_qos.entity_factory.autoenable_created_entities;
+  to_dds_qos(dds_qos.presentation, cxx_qos.presentation);
+  to_dds_qos(dds_qos.partition, cxx_qos.partition);
+  to_dds_qos(dds_qos.group_data, cxx_qos.group_data);
+  to_dds_qos(dds_qos.entity_factory, cxx_qos.entity_factory);
 }
 
 void to_cxx_qos(Rust_OpenDDS::SubscriberQos& cxx_qos, const DDS::SubscriberQos& dds_qos)
 {
-  to_cxx_qos(cxx_qos.presentation.access_scope, dds_qos.presentation.access_scope);
-  cxx_qos.presentation.coherent_access = dds_qos.presentation.coherent_access;
-  cxx_qos.presentation.ordered_access = dds_qos.presentation.ordered_access;
-
-  {
-    const CORBA::ULong len = dds_qos.partition.name.length();
-    cxx_qos.partition.name.reserve(len);
-    for (CORBA::ULong i = 0; i < len; ++i) {
-      cxx_qos.partition.name.emplace_back(rust::String(dds_qos.partition.name[i]));
-    }
-  }
-
-  {
-    const CORBA::ULong len = dds_qos.group_data.value.length();
-    cxx_qos.group_data.value.reserve(len);
-    for (CORBA::ULong i = 0; i < len; ++i) {
-      cxx_qos.group_data.value.push_back(dds_qos.group_data.value[i]);
-    }
-  }
-
-  cxx_qos.entity_factory.autoenable_created_entities = dds_qos.entity_factory.autoenable_created_entities;
+  to_cxx_qos(cxx_qos.presentation, dds_qos.presentation);
+  to_cxx_qos(cxx_qos.partition, dds_qos.partition);
+  to_cxx_qos(cxx_qos.group_data, dds_qos.group_data);
+  to_cxx_qos(cxx_qos.entity_factory, dds_qos.entity_factory);
 }
 
 void to_dds_qos(DDS::TopicQos& dds_qos, const Rust_OpenDDS::TopicQos& cxx_qos)
